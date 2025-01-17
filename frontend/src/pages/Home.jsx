21e1859f9d2489b6aa093e9d1f231c3c9621c93b
naafiisaa30 <naficste15@gmail.com>
2025-01-17 07:49:09 +0600

    feature added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,9 +6,13 @@ import heroImg02 from '../assets/images/hero-img02.jpg';
 import heroVideo from '../assets/images/hero-video.mp4';
 import Subtitle from '../shared/Subtitle';
 import worldImg from '../assets/images/world.png';
+import experienceImg from '../assets/images/experience.png'
 import SearchBar from '../shared/SearchBar';
 import ServiceList from '../Services/ServiceList';
 import FeaturedTourList from '../components/FeaturedTours/FeaturedTourList';
+import MasonryImagesGallery from '../components/Image-gallery/MasonryImagesGallery';
+import Testimonials from "../components/Testimonial/Testimonials";
+import Newsletter from "../shared/Newsletter";
 
 const Home = () => {
   return (
@@ -64,13 +68,95 @@ const Home = () => {
                    </Container>
                     </section>
 
-                    <section> <Container> 
+
+         {/* featured tour section */}
+                    <section> 
+                      <Container> 
                         <Row> 
                             <Col lg="12" className="mb-5"> 
-                            <Subtitle subtitle={"Explore"} />
+                              <Subtitle subtitle={"Explore"} />
                                 <h2 className="featured_tour-title">Our featured tours</h2> 
-                                </Col>
-                                <FeaturedTourList/> </Row> </Container> </section>
+                            </Col>
+                                <FeaturedTourList/> 
+                        </Row> 
+                      </Container>
+                    </section>
+
+          {/* experienced section */}
+          <section>
+             <Container> 
+              <Row> 
+                <Col lg="6"> 
+                   <div className="experience_content"> 
+                      <Subtitle subtitle={"Experience"} /> 
+                      
+                      <h2> With our all experience <br /> we will serve you </h2>
+
+                       <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br /> Quas aliquam, hic tempora inventore suscipit unde. </p> 
+                       
+                       </div> 
+
+                      <div className="counter_wrapper d-flex  align-items-center gap-5">
+                         <div className="counter_box"> 
+                             <span>12k+</span> 
+                             <h6>Successful Trip</h6>
+                          </div> 
+                          
+                          <div className="counter_box"> 
+                            <span>2k+</span> 
+                            <h6>Regular Clients </h6> 
+                          </div>
+                          <div className="counter_box"> 
+                            <span>15</span> 
+                            <h6>Years Experience</h6> 
+                          </div> 
+                          
+                      </div>
+                </Col> 
+                 <Col lg="6">
+                    <div className='experience_img'>
+                       <img src={experienceImg} alt="" />
+                    </div>
+                 </Col>
+              </Row> 
+            </Container>
+           </section>
+
+
+           {/* Gallery Section */}
+           <section>
+             <Container>
+                <Row>
+                  <Col lg='12'>
+                    <Subtitle subtitle = { 'Gallery'} />
+                    <h2 className="gallery_title">Visit our customers tour Gallery</h2>
+                 </Col>
+                 <Col lg='12'>
+                  <MasonryImagesGallery/>
+                 </Col>
+               </Row>
+             </Container>
+           </section>
+
+
+           {/* Testimonial Section */}
+           <section>
+            <Container>
+               <Row>
+                 <Col lg='12'>
+                    <Subtitle subtitle = { 'Fans Love'}/>
+                    <h2 className="testimonial_title">What our fans say about us</h2>
+                 </Col>
+                 <Col lg='12'>
+                  <Testimonials />
+                 </Col>
+               </Row>
+           </Container>
+          </section>
+
+
+          {/* Newsletter Section */}
+          <Newsletter/>
     </>
   );
 };
--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 import { Card, CardBody } from "reactstrap"; 
- import {Link } from "react-router-dom"; 
+ import {Link } from "react-router-dom";
+ import calculateAvgRating from '../utils/avgRating';
+
  import "./tour-card.css"
-  const TourCard = ({ tour }) => { const { id, title, city,photo, price, featured, avgRating, reviews } = tour; 
+
+
+  const TourCard = ({ tour }) => { const { id, title, city,photo, price, featured, reviews } = tour; 
+
+  const { totalRating, avgRating} = calculateAvgRating(reviews);
+
+     
+
+
   return(
-     <div className="tour_card"> <Card> <div className="tour_img"> <img src={photo} alt="tour-img" /> <span>Featured</span> </div> </Card> 
+     <div className="tour_card"> <Card> <div className="tour_img"> <img src={photo} alt="tour-img" /> {featured && <span>Featured</span> }</div> </Card> 
      <CardBody>
          <div className="card_top d-flex align-items-center justify-content-between"> <span className="tour_location d-flex align-items-center gap-1"> 
             <i class="ri-map-pin-line"></i> {city} </span> 
             <span className="tour_rating d-flex align-items-center gap-1"> 
-                <i class="ri-star-fill"></i> {avgRating}{" "} <span>({reviews.length})</span>
+            <i class="ri-star-fill"></i> {avgRating === 0 ? null: avgRating} {totalRating === 0 ? ( "Not rated" ): ( <span>({reviews.length})</span>)}
                  </span> 
                  </div>
                   <h5 className="tour_title"><Link to={`/tours/${id}`}>{title} </Link>
