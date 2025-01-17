eaea24db69e5ec0644c0225ce9493815fe966270
ssssss-code <128954534+ssssss-code@users.noreply.github.com>
2025-01-17 19:15:22 +0600

    login and register page added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -35,6 +35,7 @@ const quick_links2= [
 
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
 
     <footer className="footer" >
@@ -68,7 +69,7 @@ const Footer = () => {
           <Col lg="3">
           <h5 className="footer_link-title">Quick Links</h5>
           <ListGroup className="footer_quick-links">
-            {quick_links.map((item, index) (
+            {quick_links.map((item, index) =>(
             <ListGroupItem key={index} className="ps-0 border-0">
               <Link to={item.path}>{item.display}</Link>
             </ListGroupItem>
@@ -77,9 +78,10 @@ const Footer = () => {
         </Col>
 
 
-        <Col lg="3"><h5 className="footer_link-title">Quick Links</h5>
+        <Col lg="3">
+        <h5 className="footer_link-title">Quick Links</h5>
           <ListGroup className="footer_quick-links">
-            {quick_links2.map((item, index) (
+            {quick_links2.map((item, index) =>(
             <ListGroupItem key={index} className="ps-0 border-0">
               <Link to={item.path}>{item.display}</Link>
             </ListGroupItem>
@@ -87,8 +89,15 @@ const Footer = () => {
           </ListGroup></Col>
 
 
-        <Col lg="3"></Col>
+        <Col lg="3">
+        <h5 className="footer_link-title">Quick Links</h5>
+        <ListGroup className="footer_quick-links"> <ListGroupItem className="ps-0 border-Ø d-flex align-items-center gap-3"> <h6 className="mb-0 d-flex align-items-center gap-2"> <span> <i class="ri-map-pin-line"></i> </span> Address: </h6> <p className="mb-0">Sylhet, Bangladesh</p> </ListGroupItem> 
+        <ListGroupItem className="ps-0 border-Ø d-flex align-items-center gap-3"> <h6 className="mb-0 d-flex align-items-center gap-2"> <span> <i class="ri-mail-line"></i> </span> Email: </h6> <p className="mb-0">awe@gmail.com</p> </ListGroupItem>
+        <ListGroupItem className="ps-0 border-Ø d-flex align-items-center gap-3"> <h6 className="mb-0 d-flex align-items-center gap-2"> <span> <i class="ri-phone-line"></i> </span> Phone: </h6> <p className="mb-0">+88092771727</p> </ListGroupItem></ListGroup>
+          </Col>
 
+<Col lg='12' className="text-center pt-5">
+<p className="copyright ">CopyRight {year}. All rights reserved.</p></Col>
         </Row>
       </Container>
     </footer>
--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -7,15 +7,17 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 
 import SearchResult from '../pages/SearchResult';
+import ThankYou from '../pages/ThankYou';
 const Router = () => {
     return (
         <Routes>
             <Route path='/' element={<Navigate to='/home/'></Navigate>}></Route>
             <Route path='/home' element={<Home></Home>}></Route>
-            <Route path='/tour' element={<Tours></Tours>}></Route>
-            <Route path='/tour/:id' element={<TourDetails></TourDetails>}></Route>
+            <Route path='/tours' element={<Tours></Tours>}></Route>
+            <Route path='/tours/:id' element={<TourDetails></TourDetails>}></Route>
             <Route path='/login' element={<Login></Login>}></Route>
             <Route path='/register' element={<Register></Register>}></Route>
+            <Route path='/thank-you' element={<ThankYou></ThankYou>}></Route>
             <Route path='/tour/search' element={<SearchResult></SearchResult>}></Route>
 
         </Routes>
